577c00e024a6eba078a7af64f4b48f63b1a40e90
MellowMaiden <l2628787@gmail.com>
2023-10-24 12:09:06 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,51 @@
+# lesson # ex1
+dog = 2
+cat = -1
+print(dog)
+print(cat)
+# ex2
+x = 2 # python will automatically think x is an int
+x = 2.0 # python will automatically think x is float
+x = "two" # python will automatically think x is a string
+x = "2" # python will automatically think x is a string
+print(2)
+# exp 3
+x = 1
+y = 2
+z = x + y
+print(z)
+x = 1
+y = 2
+z = y/x
+print(z)
+ # exp 4
+x = 1
+y = 2
+z = int(y/x)
+print(z)
+x = 1 # or you can do this
+y = 2
+z = y/x
+print(int(z))
+# exp 5
+name = input("what is your name?")
+print("hello")
+print(name)
+# exp 6
+x = int (input("give me an interger:")) # converts the answer to an int
+y = int (input("give me an interger:")) # converts the answer to an int
+z = x + y
+print("the sum is:")
+print(z)
+# write a program that asks the user for the year they were born
+# then the program tells the user what birthday they will celebrate
+# this tear, for example it ask Me and i say 1984, and it tells me
+# i will celebrate my 39th brithday this year
+# exp 7
+year = int(input("tell me ,the year you were born"))
+age = 2023-year
+print("you will celerate being")
+print(age)
+print("years old this year.")
+# or you can write
+print("you will celebrate being",age,"years old this years")
